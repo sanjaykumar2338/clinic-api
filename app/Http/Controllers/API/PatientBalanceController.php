de6440dfb65f5d7c9748e4e74605ea8dd979f8f2
Sanjay Kumar <sk963070@gmail.com>
2023-09-18 00:33:41 +0530

    new changes

--- app/Http/Controllers/API/PatientBalanceController.php
+++ app/Http/Controllers/API/PatientBalanceController.php
@@ -55,7 +55,7 @@ class PatientBalanceController extends Controller
 
                 $expenses = Expenses::whereBetween('mcl_revenue.created_at',[Carbon::parse($startDate)->format('Y-m-d 00:00:00'),Carbon::parse($endDate)->format('Y-m-d 23:59:59')])->where('patient',$id)->get();
         }else{
-                $revenue = Revenue::join('mcl_revenue_patient','mcl_revenue_patient.id','=','mcl_revenue.revenue')->where('mcl_revenue_patient.patient',$id)->get();
+                $revenue = Revenue::join('mcl_revenue_patient','mcl_revenue_patient.revenue','=','mcl_revenue.id')->where('mcl_revenue_patient.patient',$id)->get();
 
                 $expenses = Expenses::where('patient',$id)->get();
         }
