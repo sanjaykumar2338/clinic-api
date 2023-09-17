b96516f9b2699b21eeb41c5abb1093ed0cbef2b4
Sanjay Kumar <sk963070@gmail.com>
2023-09-18 00:37:42 +0530

    new changes

--- app/Http/Controllers/API/PatientBalanceController.php
+++ app/Http/Controllers/API/PatientBalanceController.php
@@ -51,13 +51,13 @@ class PatientBalanceController extends Controller
                 $startDate = $request->from;
                 $endDate = $request->to;
 
-            	$revenue = Revenue::join('mcl_revenue_patient','mcl_revenue_patient.revenue','=','mcl_revenue.id')->whereBetween('created_at',[Carbon::parse($startDate)->format('Y-m-d 00:00:00'),Carbon::parse($endDate)->format('Y-m-d 23:59:59')])->where('mcl_revenue_patient.patient',$id)->select('mcl_revenue.*')->get();
+            	$revenue = Revenue::join('mcl_revenue_patient','mcl_revenue_patient.revenue','=','mcl_revenue.id')->whereBetween('created_at',[Carbon::parse($startDate)->format('Y-m-d 00:00:00'),Carbon::parse($endDate)->format('Y-m-d 23:59:59')])->where('mcl_revenue_patient.patient',$id)->select('mcl_revenue.*')->orderBy('mcl_revenue.id','desc')->get();
 
-                $expenses = Expenses::whereBetween('mcl_revenue.created_at',[Carbon::parse($startDate)->format('Y-m-d 00:00:00'),Carbon::parse($endDate)->format('Y-m-d 23:59:59')])->where('patient',$id)->get();
+                $expenses = Expenses::whereBetween('mcl_revenue.created_at',[Carbon::parse($startDate)->format('Y-m-d 00:00:00'),Carbon::parse($endDate)->format('Y-m-d 23:59:59')])->where('patient',$id)->orderBy('id','desc')->get();
         }else{
-                $revenue = Revenue::join('mcl_revenue_patient','mcl_revenue_patient.revenue','=','mcl_revenue.id')->where('mcl_revenue_patient.patient',$id)->select('mcl_revenue.*')->get();
+                $revenue = Revenue::join('mcl_revenue_patient','mcl_revenue_patient.revenue','=','mcl_revenue.id')->where('mcl_revenue_patient.patient',$id)->select('mcl_revenue.*')->orderBy('mcl_revenue.id','desc')->get();
 
-                $expenses = Expenses::where('patient',$id)->get();
+                $expenses = Expenses::where('patient',$id)->orderBy('id','desc')->get();
         }
         
         $mergedData = $revenue->concat($expenses);
