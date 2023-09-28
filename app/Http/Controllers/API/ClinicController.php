62f18a2c1e0903edbd77909521654c053a8e9085
Sanjay Kumar <sk963070@gmail.com>
2023-09-28 11:59:21 +0530

    new changes

--- app/Http/Controllers/API/ClinicController.php
+++ app/Http/Controllers/API/ClinicController.php
@@ -458,7 +458,7 @@ class ClinicController extends Controller
     public function doctor_list(Request $request){
         try{
             
-            $doctor = Doctor::with('user')->get();
+            $doctor = Doctor::with('user')->join('mcl_clinic_doctor','mcl_clinic_doctor.doctor','=','v3_doctors.id')->where('mcl_clinic_doctor.doctor',$request->user()->clinic_id)->get();
             
             $response = [
                 'success'=>true,
