b489b06d60e1e17d258849b8c57e086fdd33a637
Sanjay Kumar <sk963070@gmail.com>
2023-10-03 23:48:14 +0530

    new changes

--- app/Http/Controllers/API/PatientBalanceController.php
+++ app/Http/Controllers/API/PatientBalanceController.php
@@ -74,6 +74,15 @@ class PatientBalanceController extends Controller
         $arr = [];
         if($sortedData){
             foreach($sortedData as $row){
+                if($row->payment_purpose){
+                    $payment_purpose = Paymentpurpose::find($row->payment_purpose);
+                    if($payment_purpose){
+                        $row->comments = $payment_purpose->name;
+                    }
+                }else{
+                    $row->comments = '';
+                }
+
                 $arr[] = $row;
             }
         }
