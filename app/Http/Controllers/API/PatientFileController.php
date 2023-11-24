571429e340711681801b175518ca0aac18aabf88
Sanjay Kumar <sk963070@gmail.com>
2023-11-24 22:20:31 +0530

    new changes

--- app/Http/Controllers/API/PatientFileController.php
+++ app/Http/Controllers/API/PatientFileController.php
@@ -10,6 +10,7 @@ use App\Models\DoctorPatient;
 use Validator;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
+use DB;
 
 class PatientFileController extends Controller
 {
@@ -43,18 +44,27 @@ class PatientFileController extends Controller
 
     public function doctor_patient(Request $request, $id){
 
+        $patient_file = DoctorPatient::where('expedient_id',$id)->join('v3_patients','v3_patients.id','=','v3_doctor_patient.patient_id')->leftjoin('v3_addresses','v3_addresses.patient_id','=','v3_doctor_patient.patient_id')->select('v3_patients.*','v3_addresses.*')->first();
+
+        if(!$patient_file){
+            return response()->json(['message' => 'no patient found','success'=>false], 404);
+        }
+
+        //echo "<pre>"; print_r($patient_file); die;
+        $address = $patient_file->number.' '.$patient_file->colony.' '.$patient_file->street.' '.$patient_file->city.' '.$patient_file->state.' '.$patient_file->country;
+
         $data = [
-            'fullname' => '',
-            'gender' => '',
-            'dob' => '',
+            'fullname' => $patient_file->first_name.' '.$patient_file->last_name,
+            'gender' => $patient_file->gender,
+            'dob' => $patient_file->birth_date,
             'education' => '',
             'maritalStatus' => '',
-            'occupation' => '',
-            'placeOrigin' => '',
-            'address' => '',
+            'occupation' => $patient_file->occupation,
+            'placeOrigin' => $patient_file->city,
+            'address' => $address,
             'allergies' => [
-                'hasAllergy' => '',
-                'specificAllergy' => '',
+                'hasAllergy' => $patient_file->app_allergies !="" ? true:false,
+                'specificAllergy' => $patient_file->app_allergies,
             ],
             'age' => '',
             'caregiver' => '',
